8627173652ce8beb4bfb4e87ccbf8fddf04cfbae
Minh Dinh <dtminhpt@gmail.com>
2016-03-24 08:54:34 +0700

    Write test for 'Product.alphabetical'

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Product.alphabetical' do
+    it 'should return all products sorted by name alphabetically' do
+      product1 = Product.create!(name: 'product C')
+      product2 = Product.create!(name: 'product A')
+      product3 = Product.create!(name: 'product B')
+
+      expect(Product.alphabetical).to eq([product2, product3, product1])
+    end
+  end
 end
